fe91f882cac11a93379d5bd45f4364767f1eee6e
HoangNguyen1406 <hoangnguyen060114@gmail.com>
2025-08-06 22:56:03 +0700

    fix login và nhóm các route

--- routes/web.php
+++ routes/web.php
@@ -18,35 +18,25 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
-Route::get('/', function () {
-    return view('layouts.app');
-})->name('home');
-
-Route::get('/diem-danh', [DiemDanhController::class, 'index'])->name('diem-danh');
-
-Route::get('/dashboard', function () {
-    return view('index');
-})->name('dashboard');
-
-Route::get('/phieu-danh-gia', function () {
-    return view('phieu.index');
-})->name('phieu-danh-gia');
-
-Route::get('/lich-tuan', [LichController::class,'index'])->name('lich-tuan');
-
-Route::get('/info',[TeacherController::class,'index'])->name('info');
+// Route::get('/', function () {
+//     return view('layouts.app');
+// })->name('home');
 
 Auth::routes();
-Route::get('/login', function () {
-    return view('auth.login');
-})->name('login');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::post('/logout', function () {
-    Auth::logout();
-    return redirect('/login');
-})->name('logout');
-
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware('auth')->group(function (){
+    Route::get('/diem-danh', [DiemDanhController::class, 'index'])->name('diem-danh');
+    //--
+    Route::get('/dashboard', function () {
+        return view('index');
+    })->name('dashboard');
+    //--
+    Route::get('/phieu-danh-gia', function () {
+        return view('phieu.index');
+    })->name('phieu-danh-gia');
+    //--
+    Route::get('/lich-tuan', [LichController::class,'index'])->name('lich-tuan');
+    //--
+    Route::get('/info',[TeacherController::class,'index'])->name('info');
+});
